ffeeec0c69d3f0dbd27d6a009fb3406286ee95a3
Hui Xie <hui.742369@gmail.com>
2024-08-29 16:25:14 +0800

    Update open and close block_device scripts.

--- docker/close_block_device.sh
+++ docker/close_block_device.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
+set -eux
+set -o pipefail
+
 if [ ! $# -eq 2 ]; then
-    echo "Usage: $0 <mapper> <mount-point>"
-    exit 1
+  echo "Usage: $0 <mapper-name> <mount-point>"
+  exit 1
 fi
 
 MAPPER_NAME=$1
 MOUNT_POINT=$2
-umount "$MOUNT_POINT"
-if [ $? -eq 0 ]; then
-  echo "$MOUNT_POINT has been umounted"
-else
-  echo "fail to umount $MOUNT_POINT" 
-fi
 
-cryptsetup close "$MAPPER_NAME"
-if [ $? -eq 0 ]; then
-  echo "/dev/mapper/$MAPPER_NAME has been closed"
-else
-  echo "fail to close /dev/mapper/$MAPPER_NAME" 
-fi
-
-echo "" && echo "" && echo ""
+umount "${MOUNT_POINT}"
+cryptsetup close "${MAPPER_NAME}"
 exit 0
--- docker/open_block_device.sh
+++ docker/open_block_device.sh
@@ -1,79 +1,73 @@
 #!/bin/bash
 
-set -e
+set -eux
+set -o pipefail
 
 if [ ! $# -eq 4 ]; then
-    echo "Usage: $0 <device> <mapper> <mount-point> <password-file|password-env>"
-    exit 1
+  echo "Usage: $0 <device> <mapper> <mount-point> <password-file|password-env>"
+  exit 1
 fi
 
 DEVICE=$1
 MAPPER_NAME=$2
 MOUNT_POINT=$3
 PASSWORD=$4
-firstTime=0
+DO_FORMAT=0
 
-if [ -f "$PASSWORD" ]; then
-  DEVICE_PASSWORD=$(cat "$PASSWORD")
+if [ -f "${PASSWORD}" ]; then
+  DEVICE_PASSWORD=$(cat "${PASSWORD}")
 else
   DEVICE_PASSWORD=${!PASSWORD}
 fi
 
 if [ -z "${DEVICE_PASSWORD}" ]; then
-  echo "no DEVICE_PASSWORD"
+  echo "ERROR: device password is required"
   exit 2
 fi
 
-if cryptsetup isLuks $DEVICE; then
-    echo "Device $DEVICE is already LUKS formatted."
-    echo -n ${DEVICE_PASSWORD} | cryptsetup luksOpen $DEVICE "$MAPPER_NAME" --disable-keyring -d /dev/stdin
+# initialize, open and format
+if cryptsetup isLuks "${DEVICE}"; then
+  echo "INFO: device ${DEVICE} is already initialized as LUKS, open it directly"
+  echo -n "${DEVICE_PASSWORD}" | cryptsetup luksOpen "${DEVICE}" "${MAPPER_NAME}" --disable-keyring -d /dev/stdin
+  echo "INFO: LUKS opened"
 else
-    echo "Device $DEVICE is not LUKS formatted. Formatting now..."
-    # format
-    echo -n ${DEVICE_PASSWORD} | cryptsetup -q luksFormat --type luks2 --hash sha256 --pbkdf pbkdf2 --pbkdf-force-iterations 10000 $DEVICE -d /dev/stdin
-
-    # create fs
-    echo -n ${DEVICE_PASSWORD} | cryptsetup luksOpen $DEVICE "$MAPPER_NAME" --disable-keyring -d /dev/stdin
-    mkfs.ext4 /dev/mapper/"$MAPPER_NAME"
-    firstTime=1
+  echo "INFO:device $DEVICE is not initialized as LUSK yet, initialize, open and format it to EXT4"
+  echo -n "${DEVICE_PASSWORD}" | cryptsetup -q luksFormat --type luks2 --hash sha256 --pbkdf pbkdf2 --pbkdf-force-iterations 10000 "${DEVICE}" -d /dev/stdin
+  echo "INFO: LUKS initialized"
+  echo -n "${DEVICE_PASSWORD}" | cryptsetup luksOpen "${DEVICE}" "${MAPPER_NAME}" --disable-keyring -d /dev/stdin
+  echo "INFO: LUKS opened"
+  mkfs.ext4 "/dev/mapper/${MAPPER_NAME}"
+  echo "INFO: EXT4 formatted"
+  DO_FORMAT=1
 fi
 
 # resize if needed
-if [ $firstTime -eq 0 ]; then
-  set +e
-  echo "Resize if needed"
-  MAPPER=/dev/mapper/"$MAPPER_NAME"
-  DEVICE_SIZE=$(blockdev --getsize64 "$MAPPER")
-  if [ $? -ne 0 ]; then
-      echo "Error: Failed to get device size for $MAPPER"
-  else
-      echo "Device size (bytes): $DEVICE_SIZE"
-      FS_INFO=$(dumpe2fs -h "$MAPPER" 2>/dev/null)
-      if [ $? -ne 0 ]; then
-          echo "Error: Failed to get filesystem info for $MAPPER"
-      else
-          BLOCK_COUNT=$(echo "$FS_INFO" | grep 'Block count:' | awk '{print $3}')
-          BLOCK_SIZE=$(echo "$FS_INFO" | grep 'Block size:' | awk '{print $3}')
-          FILESYSTEM_SIZE=$((BLOCK_COUNT * BLOCK_SIZE))
-          echo "Filesystem size (bytes): $FILESYSTEM_SIZE"
-          if [ "$FILESYSTEM_SIZE" -lt "$DEVICE_SIZE" ]; then
-              echo "Filesystem is smaller than the device. Resizing..."
-              cryptsetup resize "$MAPPER"
-              echo "cryptsetup resize result $?"
-              timeout 600s e2fsck -f -y "$MAPPER"
-              echo "e2fsck result $?"
-              resize2fs "$MAPPER"
-              if [ $? -eq 0 ]; then
-                  echo "Resize successful."
-               else
-                  echo "Resize failed."
-               fi
-          else
-             echo "No need to resize. Filesystem is already equal or larger than the device."
-          fi
-      fi
+if [ ${DO_FORMAT} -eq 0 ]; then
+  echo "INFO: resize ${DEVICE} if needed"
+
+  MAPPER_DEVICE="/dev/mapper/${MAPPER_NAME}"
+  DEVICE_SIZE=$(blockdev --getsize64 "${MAPPER_DEVICE}")
+  echo "INFO: device size (bytes): ${DEVICE_SIZE}"
+
+  FS_INFO=$(dumpe2fs -h "${MAPPER_DEVICE}" 2>/dev/null)
+  BLOCK_COUNT=$(echo "${FS_INFO}" | grep 'Block count:' | awk '{print $3}')
+  BLOCK_SIZE=$(echo "${FS_INFO}" | grep 'Block size:' | awk '{print $3}')
+  FILESYSTEM_SIZE=$((BLOCK_COUNT * BLOCK_SIZE))
+  echo "INFO: filesystem size (bytes): ${FILESYSTEM_SIZE}"
+
+  if [ "${FILESYSTEM_SIZE}" -lt "${DEVICE_SIZE}" ]; then
+    echo "INFO: resize is needed"
+    cryptsetup resize "${MAPPER_DEVICE}"
+    echo "INFO: LUKS resized"
+
+    set -e
+    time 600 e2fsck -f -y "${MAPPER_DEVICE}"
+    echo "INFO: filesystem checked"
+    set +e
+
+    resize2fs "$MAPPER_DEVICE"
+    echo "INFO: filesystem resized"
   fi
-  set -e
 fi
 
 # check mount point
@@ -84,15 +78,13 @@ else
   echo "Mount point '$MOUNT_POINT' already exists."
 fi
 
-# mount 
-mount /dev/mapper/"$MAPPER_NAME" "$MOUNT_POINT"
+# mount
+mount "/dev/mapper/${MAPPER_NAME}" "${MOUNT_POINT}"
 
-# remove lost+found directory only if this was a new formatting operation (i.e., not on every script run)
-#if [ $firstTime -eq 1 ] && [ ! "$(ls "${MOUNT_POINT}" | grep 'lost\+found')" ]; then
-#  echo "remove lost+found for the first time"
-#  rm -rf "${MOUNT_POINT}/lost+found"
-#fi
+# remove lost+found directory
+# if [ ${DO_FORMAT} -eq 1 ] && [ "$(find "${MOUNT_POINT}" -maxdepth 1 -type d -name "lost+found" | grep -c "lost+found")" != "0" ]; then
+#   rm -rf "${MOUNT_POINT}/lost+found"
+# fi
 
-echo "" && echo "" && echo ""
-echo "Done. Device '$DEVICE' has been opened and mounted to '$MOUNT_POINT'."
+echo "INFO: ALL DONE. Device ${DEVICE} has been opened and mounted to ${MOUNT_POINT}"
 exit 0
